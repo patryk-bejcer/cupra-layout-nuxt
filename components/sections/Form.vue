28d417a28182bbd6f74c34ef4f537b03e5fad5a0
Patryk Bejcer <patryk.bejcer@gmail.com>
2024-01-22 13:24:25 +0100

    fix: changed images names to lowercase

--- components/sections/Form.vue
+++ components/sections/Form.vue
@@ -129,23 +129,23 @@ import type { CarOptions } from "~/types";
 const carOptions: CarOptions = {
   "cupra-formentor": {
     name: "Cupra Formentor",
-    image: "/images/cars/CUPRA-Formentor.webp",
+    image: "/images/cars/cupra-formentor.webp",
   },
   "cupra-leon": {
     name: "Cupra Leon",
-    image: "/images/cars/CUPRA-Leon.webp",
+    image: "/images/cars/cupra-leon.webp",
   },
   "cupra-leon-sportstourer": {
     name: "Cupra Leon Sportstourer",
-    image: "/images/cars/CUPRA-Leon-Sportstourer.webp",
+    image: "/images/cars/cupra-leon-sportstourer.webp",
   },
   "cupra-ateca": {
     name: "Cupra Ateca",
-    image: "/images/cars/CUPRA-Ateca.webp",
+    image: "/images/cars/cupra-ateca.webp",
   },
   "nowa-cupra-born": {
     name: "Nowa Cupra Born",
-    image: "/images/cars/CUPRA-Born.webp",
+    image: "/images/cars/cupra-born.webp",
   },
 };
 
